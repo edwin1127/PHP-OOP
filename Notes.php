e36a51f8bd50b003cb9380d79862d0f9259a0bf3
Edwin Corzo <edwin11ramirez@gmail.com>
2015-01-07 12:14:57 -0800

    added new classes

---
+++ Notes.php
@@ -0,0 +1,27 @@
+<?php  
+
+
+	class Person{
+		public height = "5'4";
+		public gender = "Male";
+		public age = "15"; 
+		public race ="Latino American";
+    }
+
+    class Dog{
+    	public gender = "Male";
+    	public type = "Terrior/Maltese";
+    	public weight = "9.8";
+    	public height = "1'1";
+
+    } 
+
+    class Food{
+    	public price ="$200.00";
+    	public brand ="Nike";
+    	public type ="Soccer Shoes"
+    }
+    
+
+
+?> 
