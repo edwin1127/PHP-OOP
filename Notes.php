e57a73ed495788aaa8a4a2be28c1d0a3fc52abf8
Edwin Corzo <edwin11ramirez@gmail.com>
2015-01-08 12:01:12 -0800

    finished completing lecture 1

--- Notes.php
+++ Notes.php
@@ -16,12 +16,52 @@
 
     } 
 
-    class Food{
+    class Shoes{
     	public price ="$200.00";
     	public brand ="Nike";
     	public type ="Soccer Shoes"
     }
-    
 
+    	function getShoes() {
+    	return "{this>brand}" .
+    	"{$this>price}";	
+    	}
+
+    	$shoe1 =new Shoes();
+    	$shoe1->brand="Nike";
+    	$shoe1->price="$200.00";
+
+    	class shirts {
+    	public size = "S,M,L,XL";
+    	public price = "$20-$40";
+    	public brand ="Hollister";
+    	}
+
+    	function getShirts(){
+    		return "{$this>brand}" .
+    		"{$this>price}";
+    	     }
+    	$shirt1 =new Shirt();
+    	$shirt1->brand = "Hollister";
+    	$shirt1->price = "$20-$40";
+
+    	class nachos{
+    		public price = "$5-$10";
+    		public meat = "beef";
+    		public salsa = "green salsa";
+    	}
+
+    	function getNachos() {
+    		return "{$this>price}" .
+    		"{$this>style}";
+    	}
+
+    	$nachos1 =new Nachos();
+    	$nachos1->meat = "beef";
+    	$nachos1->price ="$5-$10";
+
+
+
+   
 
 ?> 
